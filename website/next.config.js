81985e889eac5420da07dbdaf7dab4d08c7ccfb4
ramajeyam-mediwave <ramajeyam@mindwaveventures.com>
2024-09-07 00:52:03 +0530

    last

--- website/next.config.js
+++ website/next.config.js
@@ -2,16 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   // basePath: '/temp-staging-url',
-  basePath: '/next_js_project',
-  assetPrefix: '/next_js_project',
+  basePath: '/',
+  assetPrefix: '/',
   output: "export",
   swcMinify: true,
   images: {
-    unoptimized: true, // cant use next export otherwise
-  },
-  env: {
-    REACT_APP_SITE_KEY: "6Lf02gweAAAAANnV-fTjQXd-JoWlWUoc_SM433RD",
-    // REACT_APP_SITE_KEY: "6LcdgMIjAAAAAJMSaChAFVP9hh5aAnHZ43eHQMEW",
+    unoptimized: true,
   },
   trailingSlash: true,
 };
